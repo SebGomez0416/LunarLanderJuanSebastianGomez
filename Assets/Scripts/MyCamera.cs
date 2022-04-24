dd21e45fd79e97d374e151cae4e6baf401a2334d
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-04-24 07:43:58 -0300

    Add  camera follows ship

---
+++ Assets/Scripts/MyCamera.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offSet;
+
+    private void LateUpdate()
+    {
+      transform.position = target.position +offSet;
+      transform.LookAt(target);
+    }
+}
--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -6,34 +6,42 @@ public class ShipMovement : MonoBehaviour
     [SerializeField]private float rotationSpeed;
     private Quaternion deltaRotation;
     private Vector3 dirRotation;
+
+    [SerializeField] private float max;
+    [SerializeField] private float min;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        
     }
 
     private void FixedUpdate()
     {
        RotationMove();
        VerticalMove();
-
+       Debug.Log(rb.velocity.y);
     }
 
     void VerticalMove()
     {
         if (Input.GetKey(KeyCode.Space))
-            rb.AddForce(gameObject.transform.up * verticalSpeed * Time.deltaTime, ForceMode.Impulse);
+            rb.AddForce(gameObject.transform.up * verticalSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
     }
 
     void RotationMove()
     {
-        dirRotation.z=Input.GetAxisRaw("Horizontal");
-        dirRotation.x=Input.GetAxisRaw("Vertical");
-        deltaRotation = Quaternion.Euler( dirRotation*rotationSpeed * Time.fixedDeltaTime);
+        dirRotation.z = Input.GetAxisRaw("Horizontal");
+        dirRotation.x = Input.GetAxisRaw("Vertical");
+        deltaRotation= Quaternion.Euler(dirRotation* rotationSpeed * Time.fixedDeltaTime);
         
         rb.MoveRotation(rb.rotation * deltaRotation);
+        
+        //rb.rotation=Quaternion.Euler(Mathf.Clamp(rb.rotation.eulerAngles.x,min,max), rb.rotation.eulerAngles.y, rb.rotation.eulerAngles.z);
+        
+        
+        /*rb.rotation= Quaternion.Euler(Mathf.Clamp(rb.rotation.eulerAngles.x,min,max),
+            rb.rotation.eulerAngles.y,Mathf.Clamp(rb.rotation.eulerAngles.z,min,max));*/
+
     }
-    
-    
+
 }
