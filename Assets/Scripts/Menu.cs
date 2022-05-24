9d60869af8b0a3ab26079dc869b08b7c5e3ba1d7
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-05-24 19:26:32 -0300

    Add Credits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject credits;
+    
     public void ChangeScene(string scene)
     {
         SceneManager.LoadScene(scene);
     }
+    
+    public void ButtonCredits(bool set)
+    {
+        credits.SetActive(set);
+    }
+    
     public void Exit()
     {
         Application.Quit();
--- Assets/Scripts/Ship/MoveShip.cs
+++ Assets/Scripts/Ship/MoveShip.cs
@@ -10,11 +10,13 @@ public class MoveShip : MonoBehaviour
     public delegate void UseFuel();
     public static UseFuel OnUseFuel;
     private bool outFull;
+    private Quaternion deltaRotation;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         fire = GetComponentInChildren<ParticleSystem>();
+        Physics.gravity = new Vector3(0, -5f, 0);
     }
 
     private void OnEnable()
@@ -37,7 +39,7 @@ public class MoveShip : MonoBehaviour
     {
         if (outFull) return;
         if (!Input.GetKey(KeyCode.Space)) return;
-        rb.AddForce(gameObject.transform.up * verticalSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        rb.AddForce(gameObject.transform.up.normalized * verticalSpeed * Time.fixedDeltaTime,ForceMode.Impulse);
         fire.Play();        
         OnUseFuel?.Invoke();
     }
@@ -45,9 +47,12 @@ public class MoveShip : MonoBehaviour
     private void RotationMove()
     {
         dirRotation.z = Input.GetAxisRaw("Horizontal");
-        dirRotation.x = Input.GetAxisRaw("Vertical");       
+        dirRotation.x = Input.GetAxisRaw("Vertical");
         
-        rb.AddTorque(dirRotation* rotationSpeed * Time.fixedDeltaTime);
+        deltaRotation=Quaternion.Euler(dirRotation* rotationSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation* deltaRotation);
+        
+        // rb.AddTorque(dirRotation* rotationSpeed * Time.fixedDeltaTime);
         
         if(Vector3.Angle(Vector3.up,transform.up)>= limitRotation)
             rb.angularVelocity = Vector3.zero;
